9d520dc9232093b2d915677ed682f80777fac107
sugihara ryoto <sugihara ryoto>
2023-04-04 18:24:57 +0900

    演習１－１の修正を行いました。

--- ensyu1_1/src/ensyu1_1/ensyu1_1.java
+++ ensyu1_1/src/ensyu1_1/ensyu1_1.java
@@ -1,8 +1,17 @@
+//パッケージ名の宣言
 package ensyu1_1;
-
+/*
+ * クラス名:ensyu1_1
+ * 概要  :セミコロンが欠如しているとどうなるか
+ * 作成者:杉原綾斗
+ * 作成日:20230404
+ */
 public class ensyu1_1 {
+	//mainメソッドの宣言
 	public static void main(String[] args) {
+		//コンソール画面への出力
 		System.out.println("初めてのJavaプログラム。")
+		//コンソール画面への出力
 		System.out.println("画面に出力しています。")
 		
 	}
