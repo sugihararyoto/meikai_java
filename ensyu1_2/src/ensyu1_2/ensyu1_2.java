e22734210302bf9a1c738e26067a54175493a091
sugihara ryoto <sugihara ryoto>
2023-04-05 13:48:18 +0900

    演習１－２の修正を行いました。

--- ensyu1_2/src/ensyu1_2/ensyu1_2.java
+++ ensyu1_2/src/ensyu1_2/ensyu1_2.java
@@ -7,13 +7,13 @@ package ensyu1_2;
  * 作成日:2023.04.05
  */
 public class ensyu1_2 {
-/* 関数名：main
- * 概要：各行に1文字ずつ名前を表示するプラグラム
- * 引数：なし
- * 戻り値：なし
- * 作成者：R.Sugihara
- * 作成日：2023.04.05
- */
+	/* 関数名：main
+	 * 概要：各行に1文字ずつ名前を表示するプラグラム
+	 * 引数：なし
+	 * 戻り値：なし
+	 * 作成者：R.Sugihara
+	 * 作成日：2023.04.05
+	 */
 	public static void main(String[] args) {
 		//画面に杉と出力するためのコード
 		System.out.println("杉");
