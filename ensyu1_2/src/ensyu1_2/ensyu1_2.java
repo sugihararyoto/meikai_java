d786360b97479692015652ad63a811a65d8cdc1a
sugihara ryoto <sugihara ryoto>
2023-04-05 10:34:16 +0900

    演習の１－２を行いました。

---
+++ ensyu1_2/src/ensyu1_2/ensyu1_2.java
@@ -0,0 +1,28 @@
+//パッケージ名の宣言
+package ensyu1_2;
+/*
+ * クラス名:ensyu1_2
+ * 概要  :演習1-2クラス
+ * 作成者:R.Sugihara
+ * 作成日:2023.04.05
+ */
+public class ensyu1_2 {
+/* 関数名：main
+ * 概要：各行に1文字ずつ名前を表示するプラグラム
+ * 引数：なし
+ * 戻り値：なし
+ * 作成者：R.Sugihara
+ * 作成日：2023.04.05
+ */
+	public static void main(String[] args) {
+		//画面に杉と出力するためのコード
+		System.out.println("杉");
+		//画面に原と出力するためのコード
+		System.out.println("原");
+		//画面に綾と出力するためのコード
+		System.out.println("綾");
+		//画面に斗と出力するためのコード
+		System.out.println("斗");
+	}
+
+}
