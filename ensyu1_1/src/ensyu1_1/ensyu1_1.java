fdb764e75dd6470110de290fef7db6fc1655781a
sugihara ryoto <sugihara ryoto>
2023-04-05 09:30:59 +0900

    演習１－１の２回目の修正を行いました

--- ensyu1_1/src/ensyu1_1/ensyu1_1.java
+++ ensyu1_1/src/ensyu1_1/ensyu1_1.java
@@ -3,11 +3,18 @@ package ensyu1_1;
 /*
  * クラス名:ensyu1_1
  * 概要  :セミコロンが欠如しているとどうなるか
- * 作成者:杉原綾斗
- * 作成日:20230404
+ * 作成者:R.Sugihara
+ * 作成日:2023.04.05
  */
 public class ensyu1_1 {
-	//mainメソッドの宣言
+	/*
+	 * 関数名：main
+	 * 概要  :セミコロンが欠如しているとどうなるか
+	 * 引数  :なし
+	 * 戻り値:なし
+	 * 作成者:R.Sugihara
+	 * 作成日:2023.04.05
+	 */
 	public static void main(String[] args) {
 		//コンソール画面への出力
 		System.out.println("初めてのJavaプログラム。")
