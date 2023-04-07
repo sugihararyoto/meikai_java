4041427ec9d95677d5c1e42b3cd135f19799e848
sugihara ryoto <sugihara ryoto>
2023-04-07 10:23:03 +0900

    演習２－８の修正を行いました。

--- ensyu2_8/src/ensyu2_8/ensyu2_8.java
+++ ensyu2_8/src/ensyu2_8/ensyu2_8.java
@@ -1,6 +1,6 @@
 //パッケージ名の宣言
 package ensyu2_8;
-//乱数を生成する決まり文句をクラスの前に設置する
+//importする
 import java.util.Random;
 //キーボードからの入力を行う決まり文句をクラスの前に設置する
 import java.util.Scanner;
@@ -20,18 +20,18 @@ public class ensyu2_8 {
 	 * 作成日：2023.04.06
 	 */
 	public static void main(String[] args) {
-		//乱数を生成する
-		Random rand = new Random();
+		//乱数を生成する準備
+		Random randomNumber = new Random();
 		//キーボードからの読み込みを行う
-		Scanner stdIn = new Scanner(System.in);
-		//画面に整数値：と表示する
+		Scanner standardInput = new Scanner(System.in);
+		//画面に整数値の入力を促す
 		System.out.print("整数値：");
 		//変数firstNumberにキーボードから入力した値を代入する
-		int firstNumber = stdIn.nextInt();
-		//変数randomNumberに0~10の数字を代入し、5を引いて-5~5の範囲の値を生成する
-		int randomNumber = rand.nextInt(11) - 5;
+		int firstNumber = standardInput.nextInt();
+		//変数randomNumber-に5~5の範囲の値を生成し代入する
+		int secondNumber = randomNumber.nextInt(11) - 5;
 		//画面にキーボードから読み込んだ整数値±5の範囲の整数値を表示する
-		System.out.println("その値の±5の乱数を生成しました。それは" + (firstNumber - randomNumber) + ("です。"));
+		System.out.println("その値の±5の乱数を生成しました。それは" + (firstNumber + secondNumber) + ("です。"));
 	}
 
 }
