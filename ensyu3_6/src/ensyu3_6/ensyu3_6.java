8cd9fea04079edd306d97fa74ccf0a0693aa25ad
sugihara ryoto <sugihara ryoto>
2023-04-07 17:47:17 +0900

    演習３－６の修正を行いました。

--- ensyu3_6/src/ensyu3_6/ensyu3_6.java
+++ ensyu3_6/src/ensyu3_6/ensyu3_6.java
@@ -25,17 +25,17 @@ public class ensyu3_6 {
 		System.out.print("整数値：");
 		//変数inputNumberにキーボードから入力した値を代入する
 		int inputNumber = standardInput.nextInt();
-		//変数inputNumberが0より大きい時に実行する
+		//変数inputNumberが0より大きい場合
 		if (inputNumber > 0)
-			//変数inputNumberが10の倍数であるときに実行する
+			//変数inputNumberが10の倍数である場合
 			if (inputNumber % 10 == 0)
 				//画面にその値は10の倍数です。と表示する
 				System.out.println("その値は10の倍数です。");
-			//変数inputNumberが10の倍数ではないときに実行する
+			//変数inputNumberが10の倍数ではない場合
 			else
 				//画面にその値は10の倍数ではありません。と表示する
 				System.out.println("その値は10の倍数ではありません。");
-		//(inputNumber > 0)に当てはまらない場合に実行する
+		//変数inputNumberに正でない値が入力された場合
 		else
 			//画面に正でない値が入力されました。と表示する
 			System.out.println("正でない値が入力されました。");
