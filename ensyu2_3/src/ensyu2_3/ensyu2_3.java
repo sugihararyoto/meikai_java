b1ceab44e1567e4c19220e00d9a5a3df01016cf8
sugihara ryoto <sugihara ryoto>
2023-04-07 10:48:20 +0900

    演習２－３の修正を行いました。

--- ensyu2_3/src/ensyu2_3/ensyu2_3.java
+++ ensyu2_3/src/ensyu2_3/ensyu2_3.java
@@ -1,6 +1,6 @@
 //パッケージ名の宣言
 package ensyu2_3;
-//キーボードからの入力を行う決まり文句をクラスの前に設置する
+//importする
 import java.util.Scanner;
 /*
  * クラス名:ensyu2_3
@@ -18,11 +18,11 @@ public class ensyu2_3 {
 	 */
 	public static void main(String[] args) {
 		//キーボードからの読み込みを行う
-		Scanner stdIn = new Scanner(System.in);
-		//画面に整数値：と表示する
+		Scanner standardInput = new Scanner(System.in);
+		//画面に整数値の入力を促す
 		System.out.print("整数値：");
 		//変数inputNumberにキーボードから入力した値を代入する
-		int inputNumber = stdIn.nextInt();
+		int inputNumber = standardInput.nextInt();
 		//画面に[変数inputNumberに代入した数値]と[と入力しましたね]と表示する
 		System.out.println(inputNumber + "と入力しましたね。");
 	}
