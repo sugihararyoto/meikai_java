04ff7ea8872abd9d9e2b798cd9421cf059a783c6
sugihara ryoto <sugihara ryoto>
2023-04-11 14:56:19 +0900

    演習４－５を行いました。

---
+++ Ensyu4_5/src/ensyu4_5/Ensyu4_5.java
@@ -0,0 +1,55 @@
+//パッケージ名の宣言
+package ensyu4_5;
+//importする
+import java.util.Scanner;
+/*
+ * クラス名:Ensyu4_5
+ * 概要  :演習4-5クラス
+ * 作成者:R.Sugihara
+ * 作成日:2023.04.11
+ */
+public class Ensyu4_5 {
+	/*
+	 * 関数名：main
+	 * 概要：List4-5のinputNumber--が--inputNumberとなっていたら
+	 * どのような出力が得られるかを検討する
+	 * 引数：なし
+	 * 戻り値：なし
+	 * 作成者：R.Sugihara
+	 * 作成日：2023.04.11
+	 */
+	public static void main(String[] args) {
+		//キーボードからの入力を読み込む
+		Scanner standardInput = new Scanner(System.in);
+		
+		//画面にカウントダウンします。と表示する
+		System.out.println("カウントダウンします。");
+		//キーボードから入力した値を代入するための変数inputNumberを生成する
+		int inputNumber;
+		
+		//whileまでの文を実行する
+		do {
+			//画面に正の整数値の入力を促す
+			System.out.print("正の整数値：");
+			//変数にキーボードから入力した値を代入する
+			inputNumber = standardInput.nextInt();
+		/*
+		 * 変数inputNumberの値が0以下のあいだ
+		 * doからwhileまでを繰り返す
+		 */
+		} while (inputNumber <= 0);
+		
+		//変数inputNumberの値が0以上のあいだ繰り返す
+		while (inputNumber >= 0) {
+			/*
+			 * inputNumber--が--inputNumberとなっていたら
+			 * どのような出力が得られるか
+			 * 結果→画面に(入力した値-1)~-1まで表示される
+			 */
+			System.out.println(--inputNumber);
+		}
+		
+		
+	}
+
+}
