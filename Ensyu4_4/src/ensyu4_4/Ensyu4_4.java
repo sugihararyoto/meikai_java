83837dfac8809ff411022a4da4798a491acca398
sugihara ryoto <sugihara ryoto>
2023-04-11 14:39:54 +0900

    演習４－４を行いました。

---
+++ Ensyu4_4/src/ensyu4_4/Ensyu4_4.java
@@ -0,0 +1,53 @@
+//パッケージ名の宣言
+package ensyu4_4;
+//importする
+import java.util.Scanner;
+/*
+ * クラス名:Ensyu4_4
+ * 概要  :演習4-4クラス
+ * 作成者:R.Sugihara
+ * 作成日:2023.04.11
+ */
+public class Ensyu4_4 {
+	/*
+	 * 関数名：main
+	 * 概要：List4-4のwhile文終了時にinputNumberの値が-1になることを確認する
+	 * 引数：なし
+	 * 戻り値：なし
+	 * 作成者：R.Sugihara
+	 * 作成日：2023.04.11
+	 */
+	public static void main(String[] args) {
+		//キーボードからの入力を読み込む
+		Scanner standardInput = new Scanner(System.in);
+		
+		//画面にカウントダウンします。と表示する
+		System.out.println("カウントダウンします。");
+		//キーボードから入力した値を代入するための変数inputNumberを生成する
+		int inputNumber;
+		
+		//whileまでの文を実行する
+		do {
+			//画面に正の整数値の入力を促す
+			System.out.print("正の整数値：");
+			//変数にキーボードから入力した値を代入する
+			inputNumber = standardInput.nextInt();
+		/*
+		 * 変数inputNumberの値が0以下のあいだ
+		 * doからwhileまでを繰り返す
+		 */
+		} while (inputNumber <= 0);
+		
+		//変数inputNumberの値が0以上のあいだ繰り返す
+		while (inputNumber >= 0) {
+			//画面に変数inputNumberの値を表示する
+			System.out.println(inputNumber);
+			//変数inputNumberの値をデクリメント（値を1つ減らす）
+			inputNumber--;
+		}
+		
+		//画面に変数の値を表示し、-1になっていることを確認する。
+		System.out.println(inputNumber);
+	}
+
+}
