14b7c9332a7b1c2147798bb510e1afbe3bca4e03
sugihara ryoto <sugihara ryoto>
2023-04-27 17:56:51 +0900

    演習７－１８を修正しました。

--- Ensyu7_18/src/ensyu7_18/Ensyu7_18.java
+++ Ensyu7_18/src/ensyu7_18/Ensyu7_18.java
@@ -116,12 +116,9 @@ public class Ensyu7_18 {
 		 * 変数iの値が変数inputNumberの値になるまで繰り返す
 		 */
 		for (int i = 0; i < inputNumber;i++) {
-			//画面にarrayNumber[incrementNumber]の値を表示する
+			//画面にarrayNumber[i]の値を表示する
 			System.out.print(arrayNumber[i]);
-			/*
-			 * 変数incrementNumberの値が
-			 * 変数inputNumberの値から1を引いた値より小さい場合
-			 */
+			//変数iの値が変数inputNumberの値から1を引いた値より小さい場合
 			if (i < inputNumber - 1) {
 				//画面に,を表示する
 				System.out.print(", ");
